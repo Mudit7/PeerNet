246f24b9c7174ff45e3a9d0cf7f48984bd8f9a08
Mudit7 <muditmalpani7@gmail.com>
2019-10-04 00:40:40 +0530

    server in peer added

--- client.cpp
+++ client.cpp
@@ -9,68 +9,114 @@ string makemsg(vector<string> input_s);
 
 typedef struct sockaddr_in sockin_t;
 typedef struct sockaddr sock_t;
+
+void *servicethread(void *sock);
+
+ sockin_t serv_addr;
+//./client 3000(own port) 4000(tracker port)
 int main(int argc,char *argv[])
 {
-    int sock=-1;
-    int portNum=atoi(argv[1]);
+    int tracker_sockfd=-1;
+    int clientPortNum;
+    int serverPortNum;
+    pthread_t thread_id;
 
-
-    if(argc<2) 
+    if(argc<3) 
     {
-        cout<<"Enter the port number next time!\nExiting...\n";
-        return -1;
+        cout<<"Using defaut port numbers...\n";   
+        clientPortNum=4001;
+        serverPortNum=4000;
     }
-    
+    else
+    {
+        clientPortNum=atoi(argv[1]);
+        serverPortNum=atoi(argv[2]);
+    }
+
     // Connect to Tracker
-    
-    if((sock=connectToTracker(portNum))<0)
+    if((tracker_sockfd=connectToTracker(serverPortNum))<0)
     {
         cout<<"connect failed, exiting..";
         return -1;
     }
 
-    //loop starts here..**
-    string input;
+    //Creating peer's own server
+    int sock=-1;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error in client side\n");
+        return -1;
+    }
+   
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(clientPortNum);
+    serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
     
-    getline(cin,input);
-    vector<string> input_s = splitString(input);
-    vector<string> msg_s;
-    //attach peerid/port first always
-    //msg_s.push_back(string(sock));
 
-    if(input_s[0]=="create_user")
-    { 
-        msg_s.push_back(input_s[0]);      //cmd
-        msg_s.push_back(input_s[1]);      //user id
-        msg_s.push_back(input_s[2]);       //password
-        msg_s.push_back(to_string(portNum));   //port of the client
-        string res=makemsg(msg_s);
-        send (sock , (void*)res.c_str(), (size_t)res.size(), 0 );
+    bind(sock,(sock_t*)&serv_addr,sizeof(sock_t));
+    
+    int status=listen(sock,5);
+
+    if (pthread_create(&thread_id, NULL, servicethread, (void *)&sock) < 0)
+    {
+        perror("\n Could not create thread\n");
+    }
+    
+
+    //******************************
+    // process user requests
+    while(1)
+    {
+        string input;
+        cout<<"Enter cmd: ";
+        getline(cin,input);
+        vector<string> input_s = splitString(input);
+        if(input_s.size()<1) continue;
+
+        vector<string> msg_s;
+        //attach peerid/port first always
+        //msg_s.push_back(string(sock));
+
+        if(input_s[0]=="create_user")
+        { 
+            msg_s.push_back(input_s[0]);      //cmd
+            msg_s.push_back(input_s[1]);      //user id
+            msg_s.push_back(input_s[2]);       //password
+            msg_s.push_back(to_string(clientPortNum));   //port of the client
+            string res=makemsg(msg_s);
+            send (tracker_sockfd , (void*)res.c_str(), (size_t)res.size(), 0 );
+            cout<<"Create request sent to tracker\n";
+        }
+
+        if(input_s[0]=="upload")
+        { 
+            msg_s.push_back(input_s[0]);      //cmd
+            msg_s.push_back(input_s[1]);      //filename
+            string fileHash=getHash(input_s[1]);
+            msg_s.push_back(fileHash);        //Hash of file chunks
+            string res=makemsg(msg_s);
+            send (tracker_sockfd , (void*)res.c_str(), (size_t)res.size(), 0 );
+            cout<<"File Hash sent to tracker\n";
+        }
+
     }
 
-    if(input_s[0]=="upload")
-    { 
-        msg_s.push_back(input_s[0]);      //cmd
-        msg_s.push_back(input_s[1]);      //filename
-        string fileHash=getHash(input_s[1]);
-        msg_s.push_back(fileHash);        //Hash of file chunks
-        string res=makemsg(msg_s);
-        send (sock , (void*)res.c_str(), (size_t)res.size(), 0 );
-    }
-
-    //cout<<res.c_str();
-    while(1);
+    cout<<"client is somehow exiting\n";
     return 0;
 }
 
 vector<string> splitString(string input)
 {
     vector<string> res;
-    stringstream ss(input);
-    string token;
+    if(input.size()>2) 
+    {
+        stringstream ss(input);
+        string token;
 
-    while(getline(ss, token, ' ')) {
-        res.push_back(token);
+        while(getline(ss, token, ' ')) {
+            res.push_back(token);
+        }  
     }
     
     return res;
@@ -84,7 +130,31 @@ string makemsg(vector<string> input_s)
     return res;
 }
 
-    //fclose(f);
-    //close(sock);
+void *servicethread(void *sock_void)
+{
+    cout<<"Peer Server.. "<<endl;
+
+    int sock=*((int*)sock_void);
+    int addrlen=sizeof(serv_addr);
+    int newsockfd=accept(sock,(sock_t*)&serv_addr,(socklen_t*)&addrlen);
+        //cout<<"incoming!!\n";
+    if(newsockfd<0)
+    {
+        perror("accept");
+    }
+    char buffer[100]={0};
+    int n=0;
+    while(1)
+    {
+        while (( n = recv(newsockfd , buffer ,100, 0) ) > 0 ){
+            //hashes.push_back(buffer);
+            cout<<buffer<<endl;
+            //fwrite (buffer , sizeof (char), n, f);
+            memset (buffer, '\0', 100);
+        }
+    }
+    cout<<"Peer server somehow Exiting..\n";
+    return NULL;
+}
 
 
--- tracker.cpp
+++ tracker.cpp
@@ -6,12 +6,18 @@ using namespace std;
 
 void *servicethread(void *sock);
 
+// ./tracker 4000(tracker port)
 int main(int argc,char *argv[])
 {
+    int serverPortNum;
     if(argc<2) 
     {
-        cout<<"Enter the port number next time!\nExiting...\n";
-        return 0;
+        cout<<"Using defaut port numbers...\n";   
+        serverPortNum=4000;
+    }
+    else
+    {
+        serverPortNum=atoi(argv[1]);
     }
     int sock = 0;
     sockin_t serv_addr;
@@ -24,7 +30,7 @@ int main(int argc,char *argv[])
     }
     memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(atoi(argv[1]));
+    serv_addr.sin_port = htons(serverPortNum);
     serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
     int addrlen=sizeof(serv_addr);
@@ -48,30 +54,26 @@ int main(int argc,char *argv[])
         }
        
     }
-    // close(client_sockfd);
-    // close(sock);
-    // fclose(f);
+   
     return 0;
 }
 
 void *servicethread(void *sockNum)
 {
     cout<<"new thread created.. "<<endl;
-    // int filesize;
-    // recv(client_sockfd,&filesize,sizeof(filesize),0);
-    // FILE *f=fopen("recvFile","wb");
+    
     int sockfd=*((int*)sockNum);
-    //vector<string> hashes;
-    //cout<<sockfd<<endl;
 
-
-    char buffer[100]={0};
-    int n=0;
-    while (( n = recv(sockfd , buffer ,100, 0) ) > 0 ){
-        //hashes.push_back(buffer);
-        cout<<buffer<<endl;
-        //fwrite (buffer , sizeof (char), n, f);
-        memset (buffer, '\0', 100);
+    while(1)
+    {
+        char buffer[100]={0};
+        int n=0;
+        while (( n = recv(sockfd , buffer ,100, 0) ) > 0 ){
+          
+            cout<<buffer<<endl;
+            
+            memset (buffer, '\0', 100);
+        }
     }
     cout<<"Thread Exiting..\n";
     return NULL;
